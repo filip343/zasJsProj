75b1802b43753eb2b25b75fdf81024088117a1cd
FilipGH <81237699+filip343@users.noreply.github.com>
2023-09-27 16:55:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let headers = new Headers();
 headers.append('Content-Type', 'application/json');
 headers.append('Accept', 'application/json');
 
-headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
+headers.append('Access-Control-Allow-Origin', 'https://filip343.github.io/zasJsProj/');
 headers.append('Access-Control-Allow-Credentials', 'true');
 
 headers.append('GET', 'POST', 'OPTIONS');
