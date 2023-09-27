5efe8887b77f369ac9f48facf18eaf2b483953d6
FilipGH <81237699+filip343@users.noreply.github.com>
2023-09-27 16:54:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const imgHTML = document.querySelector('img');
 const url = 'https://lostaszic.pl/zastepstwa/zastepstwa.jpg';
-console.log(fetch(url));
+let headers = new Headers();
+
+headers.append('Content-Type', 'application/json');
+headers.append('Accept', 'application/json');
+
+headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
+headers.append('Access-Control-Allow-Credentials', 'true');
+
+headers.append('GET', 'POST', 'OPTIONS');
+
+headers.append('Authorization', 'Basic ' + base64.encode(username + ":" + password));
+
+console.log(fetch(url,{credentials: 'include',
+      method: 'GET',
+      headers: headers}));
